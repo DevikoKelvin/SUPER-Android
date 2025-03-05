ff300d8bcd736da52f38a805fc154d742d88484f
Deviko Kelvin <deviko.evangelion@gmail.com>
2025-03-05 16:48:42 +0700

    Adding SUPER API Init Instance Class

---
+++ app/src/main/java/id/erela/surveyproduct/helpers/api/InitSuperAPI.kt
@@ -0,0 +1,30 @@
+package id.erela.surveyproduct.helpers.api
+
+import com.google.gson.GsonBuilder
+import id.erela.surveyproduct.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object InitSuperAPI {
+    private val client = OkHttpClient().newBuilder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor.Level.BODY
+            else
+                HttpLoggingInterceptor.Level.NONE
+        })
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private fun getInstance(): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.SUPER_BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+        .build()
+
+    val endpoint: Endpoint = getInstance().create(Endpoint::class.java)
+}
